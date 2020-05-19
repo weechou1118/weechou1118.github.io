192d0aa64afdf533a4a3cab008094fefbc66856f
zoe <6506375+zoe1118@user.noreply.gitee.com>
2020-05-19 11:01:11 +0800

    Site updated: 2020-05-19 11:01:11

--- live2d-widget/waifu-tips.js
+++ live2d-widget/waifu-tips.js
@@ -101,7 +101,13 @@ function loadWidget(config) {
 		let text;
 		if (location.pathname === "/") { // 如果是主页
 			let now = new Date().getHours();
-			if (now > 5 && now <= 7) text="早上好！一日之计在于晨，美好的一天就要开始了。" ; else if (now> 7 && now <= 11) text="上午好！工作顺利嘛，不要久坐，多起来走动走动哦！" ; else if (now> 11 && now <= 13) text="中午了，工作了一个上午，现在是午餐时间！" ; else if (now> 13 && now <= 17) text="午后很容易犯困呢，今天的运动目标完成了吗？" ; else if (now> 17 && now <= 19) text="傍晚了！窗外夕阳的景色很美丽呢，最美不过夕阳红～" ; else if (now> 19 && now <= 21) text="晚上好，今天过得怎么样？" ; else if (now> 21 && now <= 23) text="["已经这么晚了呀，早点休息吧，晚安～"," "深夜时要爱护眼睛呀！"]; else ; } if (document.referrer !="=" "") { let referrer="new" url(document.referrer), domain="referrer.hostname.split(".")[1];" (location.hostname="==" referrer.hostname)>「${document.title.split(" - ")[0]}」`;
+            if (now > 5 && now <= 7) text="早上好！一日之计在于晨，美好的一天就要开始了。" ; 
+            else if (now> 7 && now <= 11) text="上午好！工作顺利嘛，不要久坐，多起来走动走动哦！" ; 
+            else if (now> 11 && now <= 13) text="中午了，工作了一个上午，现在是午餐时间！" ; 
+            else if (now> 13 && now <= 17) text="午后很容易犯困呢，今天的运动目标完成了吗？" ; 
+            else if (now> 17 && now <= 19) text="傍晚了！窗外夕阳的景色很美丽呢，最美不过夕阳红～" ; 
+            else if (now> 19 && now <= 21) text="晚上好，今天过得怎么样？" ; 
+            else if (now> 21 && now <= 23) text="['已经这么晚了呀，早点休息吧，晚安～","深夜时要爱护眼睛呀！']"; 
 			else if (domain === "baidu") text = `Hello！来自 百度搜索 的朋友<br>你是搜索 <span>${referrer.search.split("&wd=")[1].split("&")[0]}</span> 找到的我吗？`;
 			else if (domain === "so") text = `Hello！来自 360搜索 的朋友<br>你是搜索 <span>${referrer.search.split("&q=")[1].split("&")[0]}</span> 找到的我吗？`;
 			else if (domain === "google") text = `Hello！来自 谷歌搜索 的朋友<br>欢迎阅读<span>「${document.title.split(" - ")[0]}」</span>`;
